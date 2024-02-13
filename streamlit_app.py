1540556cb94a945b5eb20005ba33fd66c1eb5325
spoorthy2024 <159898652+spoorthy2024@users.noreply.github.com>
2024-02-13 16:24:48 +0530

    Added icons and additional breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oat meal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard Boiled free range egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oat meal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔Hard Boiled free range egg')
+streamlit.text('🥑🍞Avacodo toast')
