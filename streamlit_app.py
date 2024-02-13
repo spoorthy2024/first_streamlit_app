9afb860a95ec5e2b6144095c2a4463a9c0538a06
spoorthy2024 <159898652+spoorthy2024@users.noreply.github.com>
2024-02-13 16:27:30 +0530

    up1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oat meal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔Hard Boiled free range egg')
 streamlit.text('🥑🍞Avacodo toast')
+streamlit.header('🍌🥭Build yourown fruit smoothie🥝🍇')
