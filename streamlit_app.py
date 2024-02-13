7ba5ccaca0b2daa319dda4a0c71034b0e2800aac
spoorthy2024 <159898652+spoorthy2024@users.noreply.github.com>
2024-02-13 16:38:21 +0530

    Used Data from S3 Bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oat meal')
@@ -6,3 +7,5 @@ streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔Hard Boiled free range egg')
 streamlit.text('🥑🍞Avacodo toast')
 streamlit.header('🍌🥭Build your own fruit smoothie🥝🍇')
+my_fruits_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(fruits_list);
